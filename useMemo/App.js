d1c614fe67cdaefbfdd42df0578a7d5e623385d9
tpdus001129 <tpdus001129@gmail.com>
2022-12-09 16:59:50 +0900

    12/06 useMemo

---
+++ useMemo/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import EffectEx from "./EffectEx";
+import ContextEx from "./ContextEx";
+import ReducerEx from "./ReducerEx";
+
+const App = () => {
+  return (
+    <>
+      {/* <StateEx /> */}
+      {/* <EffectEx /> */}
+      {/* <ContextEx /> */}
+      <ReducerEx />
+    </>
+  );
+};
+
+export default App;
